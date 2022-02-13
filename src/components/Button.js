bc66d0087c4915dd22b312dd887ba5332c0bac82
Shubhi Singh <shubhisingh1506@gmail.com>
2022-02-13 20:32:46 +0530

    button page

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Button() {
+  const [idState, SetidState] = React.useState(0);
+
+  function XYZ(abc) {
+    console.log("Okay", abc);
+  }
+  function APIHandler(id) {
+    if (idState >0) {
+      fetch("https://jsonplaceholder.typicode.com/todos/" + idState)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    } else {
+      console.log("not reqd");
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={() => XYZ("Shubhi")}> Click</button>
+      <button onClick={() => APIHandler("2")}> API</button>
+      <button onClick={() => SetidState(idState + 1)}>++</button>
+      <button onClick={() => SetidState(idState - 1)}>--</button>
+      <p>{idState > -1 ? idState : "not required"}</p>
+    </div>
+  );
+}
