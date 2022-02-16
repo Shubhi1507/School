3579d13defb80a0430a8745888c1ecfd59cfa0bb
Shubhi Singh <shubhisingh1506@gmail.com>
2022-02-16 23:37:47 +0530

    ok

---
+++ src/screen/Practice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+function Practice() {
+const [practiceState , SetpracticeState] =  React.useState("")
+const[practiceNumber, SetpracticeNumber] = React.useState("")
+
+
+
+
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Practice
